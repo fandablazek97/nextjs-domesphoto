3cc9d81b7161be5962efda4017f7e60d4077f33c
fandablazek <blazek127@gmail.com>
2022-11-05 10:26:03 +0100

    initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  // Change output folder structure for static export: https://nextjs.org/docs/api-reference/next.config.js/trailing-slash
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
